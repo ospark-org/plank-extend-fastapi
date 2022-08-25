95a8f5c5a5fb8e78a7426371d3a479df2e6d497e
Grady Zhuo <gradyzhuo@gmail.com>
2022-08-25 12:12:41 +0800

    [FIX] tags error.

--- plank/descriptor/fastapi.py
+++ plank/descriptor/fastapi.py
@@ -25,7 +25,8 @@ class RouteActionDescriptor(ActionDescriptor):
         #prepare args of RoutableWrapperAction.
         extra_args = self.action_extra_args()
         extra_args["response_model"] = self.__response_model or extra_args.get("response_model")
-        extra_args["tags"] += [instance.name()]
+        if extra_args.get("tags") is not None:
+            extra_args["tags"] += [instance.name()]
 
         action = RoutableWrapperAction(path=path, end_point=end_point, **extra_args)
         if self.__unbound_response_handler is not None:
